effeefa606b7472da9f9db5a76351e054659d6da
Yash <yashmpsolanki@gmail.com>
2023-06-02 03:02:56 +0530

    implemented dot product kinda working

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from micro_nn.layers import * 
 from micro_nn.tensor import Tensor
 
-X = [[1, 3, 4], [1, 3, 4]]
+X = [[1, 3, 4], [4, 4 , 3]]
 
 # Testing of the tensor class 
 t = Tensor([1, 2, 3])
@@ -15,3 +15,9 @@ print(t * q)
 print(Tensor([2]) * Tensor([4]))
 
 print(Tensor.sample_random_normal(3, 3))
+
+
+# Currently the input params are fan_out, fan_in
+l1 = Linear(5, 3, X)
+l1.forward()
+print(l1.output)
